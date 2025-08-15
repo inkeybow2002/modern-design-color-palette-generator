c408bc085b0df278519d617877df2cf35c79a06f
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-15 11:49:49 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Modern Design Color Palette Generator</h1>
+      <p>Generate and explore beautiful color palettes!</p>
+    </div>
+  );
+}
+
+export default App;
